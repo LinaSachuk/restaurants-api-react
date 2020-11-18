2cf52072d7ab8461ed26770a8dbeb80a6fa872cf
Lina Sachuk <sachuklina@gmail.com>
2020-11-17 23:04:36 -0700

    add project folder

---
+++ restaurants-api/src/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+
+/**
+ * App
+ *
+ * Simple react js fetch example
+ */
+class App extends React.Component {
+
+
+
+
+
+  constructor(props) {
+
+    super(props);
+
+    this.state = {
+      items: [],
+      isLoaded: false
+    }
+
+  }
+
+  /**
+   * componentDidMount
+   *
+   * Fetch json array of objects from given url and update state.
+   */
+  componentDidMount() {
+
+    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      headers: {
+        Authorization: "Api-Key q3MNxtfep8Gt",
+      },
+    })
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          items: json,
+          isLoaded: true,
+
+        })
+      }).catch((err) => {
+        console.log(err);
+      });
+
+  }
+
+  /**
+   * render
+   *
+   * Render UI
+   */
+  render() {
+
+    const { isLoaded, items } = this.state;
+    console.log(items);
+    // address1: "201 Waterfront St"
+    // attire: "business casual"
+    // city: "Oxon Hill"
+    // genre: "Steak,American,Contemporary,Seafood,Cafe"
+    // hours: "Open Daily 5:30 PM-10:00 PM"
+    // id: "f223fdd0-4adc-423e-9747-980a66c256ca"
+    // lat: "38.782098"
+    // long: "-77.017492"
+    // name: "Old Hickory Steakhouse"
+    // state: "MD"
+    // tags: "Social,Food and Dining,Restaurants,Steakhouses"
+    // telephone: "(301) 965-4000"
+    // website: "http://www.gaylordnational.com"
+    // zip: "20745"
+    if (!isLoaded)
+      return <div>Loading...</div>;
+
+    return (
+      <div className="App">
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              Name: {item.name} | City: {item.city}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+
+  }
+
+}
+
+export default App;
